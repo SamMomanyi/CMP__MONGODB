bec6c795ad6f4fe3763684d1d616594f7fe35ddd
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-01-20 22:29:22 +0300

    first commit

---
+++ composeApp/src/commonMain/kotlin/org/example/project/app/App.kt
@@ -0,0 +1,59 @@
+package org.example.project.app
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
+import org.jetbrains.compose.resources.painterResource
+
+import cmp_todo_mongodb.composeapp.generated.resources.Res
+import cmp_todo_mongodb.composeapp.generated.resources.compose_multiplatform
+import org.example.project.Greeting
+import org.example.project.presentation.screen.home.HomeScreen
+
+
+val lightRedColor = Color(color = 0xFFF57D88)
+val darkRedColor = Color(color = 0xFF77000B)
+@Composable
+@Preview
+fun App() {
+    val lightColors = lightColorScheme(
+        primary = lightRedColor,
+        onPrimary = darkRedColor,
+        primaryContainer = lightRedColor,
+        onPrimaryContainer = darkRedColor
+    )
+
+    val darkColors = darkColorScheme(
+        primary = lightRedColor,
+        onPrimary = darkRedColor,
+        primaryContainer = lightRedColor,
+        onPrimaryContainer = darkRedColor
+    )
+
+    val colors by mutableStateOf(
+        if(isSystemInDarkTheme()) darkColors else lightColors
+    )
+    MaterialTheme(colorScheme = colors) {
+
+        Navigator(HomeScreen()){
+            SlideTransition(it)
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/screen/screen/TaskScreen.kt
@@ -0,0 +1,11 @@
+package org.example.project.presentation.screen.screen
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class TaskScreen : Screen {
+    @Composable
+    override fun Content(){
+
+    }
+}
